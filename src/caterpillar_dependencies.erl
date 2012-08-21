044e442f12b6784c386eeef0f11cf8abe3e494e2
Vasiliy Avdiushkin <vavdiushkin@smprc.ru>
2012-08-21 11:43:59 +0400

    fixed spec errors

--- src/caterpillar_dependencies.erl
+++ src/caterpillar_dependencies.erl
@@ -1,8 +1,8 @@
 -module(caterpillar_dependencies).
 -export([check_list/3]).
 
--spec check_list(dets(), caterpillar:revdef(), [caterpillar:revdef()]) ->
-    {ok, [{depends, []}, {is_dependencie, []}]}.
-check_list(DepTree, Candidate, [InProgress]) ->
+-spec check_list(pid(), caterpillar:revdef(), [caterpillar:revdef()]) -> 
+    {ok, {depends, []}, {is_dependencie, []}}.
+check_list(_DepTree, _Candidate, _CheckList) ->
     %% TODO
     {error, not_implemented}.
