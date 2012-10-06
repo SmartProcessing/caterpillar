cfe09ac33a78004346d46e87f56556ebf5e6b8b1
dmazurin@smprc.ru <>
2012-10-06 21:41:00 +0400

    caterpillar_buildnet_handler removed, caterpillar_worker skeleton

---
+++ include/caterpillar_worker_internal.hrl
@@ -0,0 +1,9 @@
+-ifndef(caterpillar_worker_internal).
+-define(caterpillar_worker_internal, true).
+
+-record(state, {
+    ident :: atom()
+}).
+
+
+-endif.
--- src/caterpillar_worker.erl
+++ src/caterpillar_worker.erl
@@ -1,121 +1,56 @@
 -module(caterpillar_worker).
--include("caterpillar.hrl").
+
+-include_lib("caterpillar_worker_internal.hrl").
+
 -behaviour(gen_server).
--define(DEFAULT_BUILD_PATH, "/srv/caterpillar").
 
--record(state, {
-    build_plugins       :: [{atom(), list()}],
-    platform_plugins    :: [{atom(), list()}],
-    build_path          :: list()
-}).
+-export([start_link/1, stop/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--export([start_link/1, start_link/0]).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
 
-start_link() ->
-    gen_server:start_link(?MODULE, [], []).
+start_link(Args) ->
+    case proplists:get_value(ident, Args, '$none') of
+        '$none' ->
+            error_logger:error_msg("no ident given, stopping~n"),
+            {error, no_ident};
+        Ident when is_atom(Ident) ->
+            gen_server:start_link({local, Ident}, ?MODULE, Args, []);
+        Ident ->
+            error_logger:error_msg("bad ident type: ~p~n", [Ident]),
+            {error, bad_ident}
+    end.
 
-start_link(Settings) ->
-    gen_server:start_link(?MODULE, [Settings], []).
 
-init(Settings) ->
-    BuildPlugins = ?GV(
-        build_plugins, 
-        Settings, 
-        [{deb, caterpillar_deb_plugin}]
-    ),
-    PlatformPlugins = ?GV(
-        platform_plugins, 
-        Settings, 
-        [{default, caterpillar_default_builder}]
-    ),
-    BuildPath = ?GV(
-        build_path, 
-        Settings, 
-        ?DEFAULT_BUILD_PATH 
-    ),
-    {ok, #state{
-        build_plugins=BuildPlugins,
-        platform_plugins=PlatformPlugins,
-        build_path=BuildPath
-    }}.
+stop(Ident) ->
+    gen_server:call(Ident, stop, infinity).
 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
 
-handle_cast({build, ToBuild}, State) ->
-    build_rev(ToBuild, State),
-    {noreply, State};
-handle_cast(_Msg, State) ->
-    {noreply, State}.
 
-handle_info(_Info, State) ->
-    {noreply, State}.
 
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
+init(Args) ->
+    State = #state{
+        ident = proplists:get_value(ident, Args)
+    }, 
     {ok, State}.
 
 
--spec build_rev(#rev_def{}, #state{}) -> {ok, term()}.
-build_rev(ToBuild, State) ->
-    PlatformPlugins = State#state.platform_plugins,
-    BuildPlugins = State#state.build_plugins,
-    BuildPath = State#state.build_path,
-    Funs = [
-        {fun unpack_rev/2, BuildPath},
-        {fun get_pkg_info/2, BuildPath},
-        {fun platform_clean/2, PlatformPlugins},
-        {fun platform_test/2, PlatformPlugins},
-        {fun platform_clean/2, PlatformPlugins},
-        {fun platform_prebuild/2, PlatformPlugins},
-        {fun build_prepare/2, BuildPlugins},
-        {fun build_check/2, BuildPlugins},
-        {fun build_submit/2, BuildPlugins}
-    ],
-    case catch caterpillar_utils:build_pipe(Funs, ToBuild) of
-        {ok, Info} ->
-            ok = gen_server:call(caterpillar, 
-                {built, self(), ToBuild, Info});
-        {error, Info} ->
-            ok = gen_server:call(caterpillar, 
-                {err_built, self(), ToBuild, Info});
-        _Other ->
-            Info = unknown_error,
-            gen_server:call(caterpillar,
-                {
-                    err_built, 
-                    self(), 
-                    ToBuild, #build_info{}
-                })
-    end,
-    {ok, Info}.
+
+handle_info(_Msg, State) -> 
+    {noreply, State}.
 
 
-unpack_rev(_,_) ->
-    {ok, none}.
+handle_cast(_Msg, State) ->
+    {noreply, State}.
 
-get_pkg_info(_BuildPath, _Revision) ->
-    {ok, none}.
 
-platform_clean(_PkgInfo, _Plugins) ->
-    {ok, none}.
+handle_call(_Msg, _From, State) ->
+    {reply, {error, bad_msg}, State}.
 
-platform_test(_PkgInfo, _Plugins) ->
-    {ok, none}.
 
-platform_prebuild(_PkgInfo, _Plugins) ->
-    {ok, none}.
+terminate(Reason, #state{ident=Ident}) ->
+    error_logger:info_msg("worker(~p) down with reason: ~p~n", [Ident, Reason]).
 
-build_prepare(_PkgInfo, _Plugins) ->
-    {ok, none}.
 
-build_check(_PkgInfo, _Plugins) ->
-    {ok, none}.
-
-build_submit(_PkgInfo, _Plugins) ->
-    {ok, none}.
+code_change(_Old, State, _Extra) ->
+    {ok, State}.
