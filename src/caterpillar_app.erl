8ce840a1b15ea399e7d96ab5716f306057195ed7
dmazurin@smprc.ru <>
2012-10-11 00:01:47 +0400

    app file fix

--- src/caterpillar_app.erl
+++ src/caterpillar_app.erl
@@ -2,7 +2,7 @@
 
 -behaviour(application).
 
--export([start/0, stop/0]).
+-export([start/0, stop_node/1, status_node/1]).
 -export([start/2, stop/1]).
 
 
@@ -10,8 +10,35 @@ start() ->
     application:start(caterpillar).
 
 
-stop() ->
-    application:stop(caterpillar).
+stop_node([Node]) ->
+    case net_adm:ping(Node) of
+        pong ->
+            rpc:call(Node, init, stop, []),
+            io:format("stopped~n");
+        pang ->
+            io:format("down or unknown~n")
+    end.
+
+
+status_node([Node]) ->
+    case net_adm:ping(Node) of
+        pong ->
+            Pid = rpc:call(Node, os, getpid, [], 10000),
+            io:format("up at pid ~s~n", [Pid]),
+            case global:whereis_name(caterpillar_event) of
+                undefined -> ok;
+                Pid -> 
+                    io:format(
+                        "registered services ~p~n", 
+                        [gen_server:call(Pid, get_info, infinity)]
+                    )
+            end;
+        pang ->
+            io:format("Node ~p is down or bad cookie ~p~n",
+            [Node, erlang:get_cookie()]
+        )
+    end.
+
 
 
 start(_StartType, _StartArgs) ->
