92942d6cba90139402ad98f0760d44df19a60d4c
dmazurin@smprc.ru <>
2012-10-24 15:17:57 +0400

    api

--- src/caterpillar_api.erl
+++ src/caterpillar_api.erl
@@ -7,11 +7,15 @@
 -export([init/3, handle/2, terminate/2]).
 -export([start_link/1]).
 
+-record(state, {ets}).
+
 
 start_link(Args) ->
     ensure_started(cowboy),
+    Ets = ets=ets:new(?MODULE, [public]),
+    State = #state{ets=Ets},
     Dispatch = [
-        {'_', [{'_', ?MODULE, []}]}
+        {'_', [{'_', ?MODULE, State}]}
     ],
     Host = proplists:get_value(host, Args, "127.0.0.1"),
     Port = proplists:get_value(port, Args, 8088),
@@ -28,26 +32,40 @@ ensure_started(App) ->
     end.
 
 
-init({tcp, http}, Req, _Opts) ->
-    {ok, Req, []}.
+init({tcp, http}, Req, State) ->
+    {ok, Req, State}.
 
 
 
-handle(#http_req{path=[Cmd, Package, Branch]}=Req, State)
+handle(#http_req{path=[Cmd, Package, Branch]}=Req, #state{ets=Ets}=State)
   when Cmd == <<"rescan">>; Cmd == <<"rebuild">>
 ->
-    AtomCmd = binary_to_atom(<<Cmd/binary, "_package">>, latin1),
-    Msg = {AtomCmd, {binary_to_list(Package), binary_to_list(Branch)}},
-    NewReq = case catch caterpillar_event:sync_event(Msg) of
-        ok -> 
-            {ok, Req2} = cowboy_http_req:reply(200, [], <<"ok">>, Req),
-            Req2;
-        Error ->
-            Response = list_to_binary(io_lib:format("~p~n", [Error])),
-            {ok, Req2} = cowboy_http_req:reply(500, [], Response, Req),
-            Req2
+    Response = (catch begin
+        case ets:lookup(Ets, {Package, Branch}, self()) of
+            [] -> ets:insert(Ets, {{Package, Branch}, self()});
+            _ -> exit(in_process)
+        end,
+        AtomCmd = binary_to_atom(<<Cmd/binary, "_package">>, latin1),
+        Msg = {AtomCmd, {binary_to_list(Package), binary_to_list(Branch)}},
+        NewReq = case catch caterpillar_event:sync_event(Msg) of
+            {ok, _Pid} -> 
+                {ok, Req2} = cowboy_http_req:reply(200, [], <<"ok">>, Req),
+                {ok, Req2};
+            Error ->
+                Res = list_to_binary(io_lib:format("~p~n", [Error])),
+                {ok, Req2} = cowboy_http_req:reply(500, [], Res, Req),
+                {ok, Req2}
+        end
+    end),
+    case Response of
+        {ok, _} -> Response;
+        Err->
+            error_logger:error_msg("api: handle ~p error: ~p~n", [Cmd, Err]),
+            {ok, R} = cowboy_http_req:reply(500, [], Response, Req),
+            {ok, R}
     end,
-    {ok, NewReq, State};
+    ets:delete(Ets, {Package, Branch}),
+    {ok, Response, State};
 
 handle(Req, State) ->
     {ok, Req2} = cowboy_http_req:reply(400, [], <<"bad request">>, Req),
@@ -55,5 +73,5 @@ handle(Req, State) ->
 
 
 
-terminate(_Req, _State) ->
+terminate(_Req, State) ->
     ok.
