a4f4466f2f37bf96d18ec8c79bc63be17d026149
Vasiliy Avdiushkin <vavdiushkin@smprc.ru>
2012-10-26 11:44:31 +0400

    rm unneeded logs

--- src/caterpillar_pkg_utils.erl
+++ src/caterpillar_pkg_utils.erl
@@ -65,9 +65,6 @@ pack_rev_def(Archive, PkgRecord) ->
 
 
 get_pkg_config_list(Path) ->
-    error_logger:info_msg("PKG_CONFIG PATH: ~p~n", [filename:join(Path, "pkg.config")]),
-    error_logger:info_msg("PKG_CONFIG DIR_LIST: ~p~n", [file:list_dir(Path)]),
-    error_logger:info_msg("PKG_CONFIG FILE: ~p~n", [catch file:consult(filename:join(Path, "pkg.config"))]),
     case [
             catch file:consult(filename:join(Path, "pkg.config")),
             filelib:is_file(Path ++ "control") ] of
