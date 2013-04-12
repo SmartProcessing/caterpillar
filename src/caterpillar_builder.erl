08414e744d46f40491ae9f4169aaae8d39f7056c
Vasiliy Avdiushkin <vavdiushkin@smprc.ru>
2013-04-12 17:23:43 +0400

    package name in message

--- src/caterpillar_builder.erl
+++ src/caterpillar_builder.erl
@@ -98,7 +98,8 @@ handle_call({built, Worker, RevDef, BuildInfo}, _From, State) ->
             binary_to_list(RevDef#rev_def.branch),
             binary_to_list(RevDef#rev_def.tag)
         ]),
-    notify(Subj, "ok"),
+    Message = io_lib:format("built package: ~s", [BuildInfo#build_info.pkg_name]),
+    notify(Subj, Message),
     NewWorkers = release_worker(Worker, State#state.workers),
     {ok, ScheduledState} = schedule_build(State#state{workers=NewWorkers}),
     {ok, NewState} = try_build(ScheduledState),
