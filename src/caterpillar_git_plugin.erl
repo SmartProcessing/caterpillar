e1dab48fca7256ad28aafd457569e466eb94b22a
Dmitriy Mazurin <dmazurin@smprc.ru>
2013-04-15 17:06:24 +0400

    type fix

--- src/caterpillar_git_plugin.erl
+++ src/caterpillar_git_plugin.erl
@@ -25,7 +25,7 @@ terminate_plugin(_State) ->
 export_archive(_State, Package, Branch, _Revno, ExportPath) ->
     ExportBranch = format("git archive heads/~s -o ~s", [Branch, ExportPath]),
     case command(ExportBranch, [{cd, Package}]) of
-        {ok, <<>>} -> ok;
+        {ok, <<>>} -> {ok, tgz};
         Error -> {error, Error}
     end.
 
