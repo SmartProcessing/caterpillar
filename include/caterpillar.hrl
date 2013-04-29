1a690e8639bdea0207f71bd5a7a6c10f482f4ef4
Dmitriy Mazurin <dmazurin@smprc.ru>
2013-04-29 15:49:13 +0400

    caterpillar hrl

--- include/caterpillar.hrl
+++ include/caterpillar.hrl
@@ -1,4 +1,7 @@
+-ifndef(caterpillar_hrl).
+-define(caterpillar_hrl, true).
 -define(GV, proplists:get_value).
+
 
 -record(archive, {
     name            :: string(), %name of package
@@ -38,3 +41,6 @@
     pre_deploy_actions = [],
     post_deploy_actions = []
 }).
+
+
+-endif.
