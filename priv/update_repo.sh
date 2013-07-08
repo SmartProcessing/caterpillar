9ce3bf76ef28ab9282cbe3add11f6f374f42b3dc
Dmitriy Mazurin <dmazurin@smprc.ru>
2013-07-08 23:21:01 +0400

    debug logs removed, exit on usage()

--- priv/update_repo.sh
+++ priv/update_repo.sh
@@ -4,6 +4,7 @@ usage(){
 cat << EOF
 usage: $0 -t=TYPE -b=BRANCH -a=ARCH
 EOF
+exit 1
 }
 
 TYPE=
@@ -23,19 +24,13 @@ while getopts "a:b:t:" OPT; do
             ;;
         ?)
             usage
-            exit 1
             ;;
     esac
 done
 
 
 if [[ -z $TYPE ]] || [[ -z $BRANCH ]] || [[ -z $ARCH ]]; then
-    echo $TYPE
-    echo $BRANCH
-    echo $ARCH
-    echo "something not right"
     usage
-    exit 1
 fi
 
 cd /srv/packages/smprc/dists/$TYPE/$BRANCH/main/binary-amd64
