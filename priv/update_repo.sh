304ec7e901b147a4ce9b2e271840717978a9af40
Dmitriy Mazurin <dmazurin@smprc.ru>
2013-07-08 23:10:03 +0400

    update_repo.sh with getopts parsing

--- priv/update_repo.sh
+++ priv/update_repo.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
-BRANCHES="
-testing
-stable
-unstable
-"
+usage(){
+cat << EOF
+usage: $0 -t=TYPE -b=BRANCH -a=ARCH
+EOF
+}
 
-for branch in $BRANCHES; do
-    cd /srv/packages/smprc/dists/$branch/main/binary-amd64
-    dpkg-scanpackages . | gzip -c9 > Packages.gz
+TYPE=
+BRANCH=
+ARCH=
+
+while getopts "a:b:t:" OPT; do
+    case $OPT in
+        "a")
+            ARCH=$OPT
+            ;;
+        "b")
+            BRANCH=$OPT
+            ;;
+        "t")
+            TYPE=$OPT
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
 done
+
+
+if [[ -z $TYPE ]] || [[ -z $BRANCH ]] || [[ -z $ARCH ]]; then
+    echo $TYPE
+    echo $BRANCH
+    echo $ARCH
+    echo "something not right"
+    usage
+    exit 1
+fi
+
+cd /srv/packages/smprc/dists/$TYPE/$BRANCH/main/binary-amd64
+dpkg-scanpackages . | gzip -c9 > Packages.gz
